5a5214203c0318ffe6253d58f1acf7b6efe2a8a8
Shruthiiii <wtffshru69@gmail.com>
2026-03-02 11:30:32 +0530

    update api url to production

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://job-tracker-api-1-sa7a.onrender.com/api',
 });
 
 // Automatically attach token to every request
